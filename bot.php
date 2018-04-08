22714d8976dcae8e40855e9ed48e7f56d2c6d8ba
Ayana.shahab <37789259+AyanaShb@users.noreply.github.com>
2018-04-09 02:56:32 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -818,9 +818,13 @@ if($msg_type == 'text'){
     $filter = explode(' ', $pesan_datang);
     if($filter[0] == 'apakah') {
         $balas = send(jawabs(), $replyToken);
-    }
-    }
-
+    } else {}
+if($msg_type == 'text'){
+    $pesan_datang = strtolower($message['text']);
+    $filter = explode(' ', $pesan_datang);
+    if($filter[0] == 'Apakah') {
+        $balas = send(jawabs(), $replyToken);
+    } else {}
 if($message['type']=='text') {
 	    if ($command == '/cuaca') {
         $result = cuaca($options);
